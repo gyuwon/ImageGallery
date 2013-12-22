a82fc22553bbafa97f45e84b3e8dd94f4447434b
Gyuwon <gyuwon@live.com>
2013-12-22 10:13:15 +0900

    ImageContent model

---
+++ ImageGallery/Models/ImageContent.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageGallery.Models
+{
+    public class ImageContent
+    {
+        [Key]
+        public int ContentId { get; set; }
+        [Required]
+        public string UserId { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+    }
+}
